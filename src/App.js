207794202e1a9d1511614cedc28671c961a0ed2a
nk-mart <113395569+nk-mart@users.noreply.github.com>
2023-03-15 20:34:45 +0000

    Update APPjs
    MVP run
    Wireframe

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import QuoteAPI from "./components/QuoteAPI";
 function App() {
   return (
     <div className="App">
+      <div className="header">Quote Collector</div> 
       
-  hello
-    
-      <QuoteAPI></QuoteAPI>
+        <QuoteAPI></QuoteAPI>
+
+      <div className="topRightButtons">
+          <a href="" className="saveIcon">save</a>
+          <a href="" className="collectionIcon">collection</a>
+      </div>
+      <div className="middleSideButtons">
+          <a href="" className="rightIcon">right</a>
+          <a href="" className="leftIcon">left</a>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
