4aa14c5759a9a322bde439f5a505bf30d8758223
Leonawesley <leonajenishlin@gmail.com>
2023-03-17 19:04:19 +0000

    getting ittems from local storage

--- src/components/CollectionPage.js
+++ src/components/CollectionPage.js
@@ -1,8 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
 function CollectionPage(){
+
+    const [savedQuotes, setSavedQuotes] = useState([]);
+    
+      useEffect(() => {
+        const quotes = Object.values(localStorage);
+        setSavedQuotes(quotes);
+        console.log(quotes);
+      }, []);
+    
     return (
         <div>
             hello
         </div>
-    )};
+     );
 
-    export default CollectionPage
+}
+
+export default CollectionPage
