963554b6a850fd9ae18cf2cb9ad58bc26e689238
Lawzstopwar <lawzstopwar@gmail.com>
2023-03-19 11:55:49 +0000

    index.html in progress

---
+++ src/components/Home.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import QuoteAPI from "./QuoteAPI.js";
+
+
+
+function collectionpage() {
+
+  const [quote, setQuote] = useState('');
+  
+
+  useEffect(() => {
+    
+
+    fetchQuote();
+  }, []);
+  
+  async function fetchQuote(event) {
+    event.preventDefault();
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+     setQuote(data.content);
+    
+    
+  }
+
+  function storeQuote(event){
+    event.preventDefault();
+    localStorage.setItem(quote,JSON.stringify(quote));
+  }
+
+  return (
+    <div className="collectionpage">
+      <div className="header">Quote Collector</div> 
+      
+        <QuoteAPI quote={quote}></QuoteAPI>
+
+      <div className="topRightButtons">
+          <a href="" className="saveIcon" onClick={storeQuote}>Save</a>
+          <a href="" className="collectionpage">collection</a>
+      </div>
+      <div className="middleSideButtons">
+          <a href="" className="rightIcon" onClick={fetchQuote}>Next Quote</a>
+      </div>
+    </div>
+  );
+}
+
+
+export default collectionpage;
