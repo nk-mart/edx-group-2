dcced6ce412548896c334a80fdf621793bebfd00
Angel <rachel@Rachels-MacBook-Air.local>
2023-03-22 22:40:15 +0000

    omved lawrence's copy idea to individual cards, added toastify for notification

--- src/components/CollectionPage/CollectionPage.js
+++ src/components/CollectionPage/CollectionPage.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './CollectionPage.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 function CollectionPage(){
 
@@ -24,17 +28,26 @@ function CollectionPage(){
           "rounded-lg h-32 transition ease-in-out delay-150 border-4 border-indigo-300/50 hover:scale-110 hover:text-white hover:bg-indigo-500 duration-300 overflow-ellipsis overflow-hidden " 
           quote-key={quoteObject.quote} 
           style={{ backgroundColor }}>
-            <FontAwesomeIcon icon="fa-thin fa-face-smile" />{quoteObject.quote} </div>)
-      }
+            {quoteObject.quote} 
+            <button onClick={() => {
+              navigator.clipboard.writeText(quoteObject.quote);
+              toast.success("Quote copied to clipboard!");
+            }}>Copy</button>
+        </div>)
+    }
     
     return (
       <div className="bg-local bg-cover background-image bg-no-repeat my-6 py-4 mx-7 shadow-xl shadow-cyan-500/50 ">
         <h1 className=" flex justify-center text-center rounded-lg py-2 px-2 lg:ml-[700px] sm:ml-[200px] md:ml-[250px] font-bold text-xl max-w-lg bg-gradient-to-r from-indigo-600 to-pink-500 text-white">Favorite quotes</h1>
         <div className="grid grid-cols-2 sm:grid-cols-4 gap-3 text-center py-2 px-2 ">         
-            {/* <div className="colourOverlay" style={{ backgroundColor }}></div> */}
             {savedQuotes.map((quote) => generatingQuote(quote))}
-          </div>
+        </div>
+        <ToastContainer />
       </div>
      );
-    }
-export default CollectionPage
+}
+
+export default CollectionPage;
+
+
+
