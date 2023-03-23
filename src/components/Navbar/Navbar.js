a329671dd5a89cf6c1fe9862ea1f38e6c0f2d453
nk-mart <113395569+nk-mart@users.noreply.github.com>
2023-03-23 19:37:34 +0000

    Update nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { MenuItems } from "./MenuItems";
+import { Link } from "react-router-dom";
 
 function Navbar (props) {
     function copyToClipBoard() {
@@ -17,8 +18,12 @@ function Navbar (props) {
         <nav className="bg-gray-800 justify-center">
             
             <ul className="flex flex-row justify-center ">
-                <li className="p-4 hover:text-white">Home</li>
-                <li className="p-4 hover:text-white">Collection</li>
+                <li className="p-4 hover:text-white">
+                <Link to="/">Collection</Link>
+                </li>
+                <li className="p-4 hover:text-white">
+                <Link to="/collectionpage">Collection</Link>
+                </li>
             </ul>
             
                
