7cb9f31c88a58c0cd8f7ae6f736af15333d06291
nk-mart <113395569+nk-mart@users.noreply.github.com>
2023-03-23 20:10:44 +0000

    nav bar update
    nav bar update

--- src/components/CollectionPage/CollectionPage.js
+++ src/components/CollectionPage/CollectionPage.js
@@ -7,40 +7,28 @@ import "../Button.css"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-function CollectionPage() {
+function CollectionPage(){
 
   const [savedQuotes, setSavedQuotes] = useState([]);
-
+  
   useEffect(() => {
     const quotes = JSON.parse(localStorage.getItem('quotes')) || [];
     setSavedQuotes(quotes);
     console.log(quotes)
   }, []);
-
-  function copyQuote(quote) {
-    navigator.clipboard.writeText(quote);
-
-    // Alert the copied text
-    alert("Copied the text: " + quote);
-  }
-
-
-  function generatingQuote(quote) {
+  
+  function generatingQuote(quote){
     let quoteObject = quote;
     if (typeof quote === 'string') {
       quoteObject = JSON.parse(quote);
     }
     const backgroundColor = quoteObject.color;
     return (
-      <>
-        <div className=
+      
+      <div className=
           "rounded-lg h-32 transition ease-in-out delay-150 border-4 border-indigo-300/50 hover:scale-110 hover:text-white hover:bg-indigo-500 duration-300 overflow-ellipsis overflow-hidden " 
           quote-key={quoteObject.quote} 
           style={{ backgroundColor }}>
-          
-            <span>Copy</span>
-          </button>
-          <p>{quoteObject.quote}</p>
                <Button
               functionToDo={() => {
                 navigator.clipboard.writeText(quoteObject.quote);
@@ -49,13 +37,15 @@ function CollectionPage() {
               content="Copy"
               className="copyButton"
             />
-            {quoteObject.quote} 
-         
-        </div>
+            {quoteObject.quote}
+            </div>
+           
       );
     }
     
     return (
+      <>
+        <Navbar></Navbar>
       <div className="card bg-local bg-cover background-image bg-no-repeat my-6 py-4 mx-7 shadow-xl shadow-cyan-500/50 ">
         <h1 className="title flex justify-center text-center rounded-lg py-2 px-2 lg:ml-[700px] sm:ml-[200px] md:ml-[250px] font-bold text-xl max-w-lg bg-gradient-to-r from-indigo-600 to-pink-500 text-white">Favorite quotes</h1>
         <div className="grid grid-cols-2 sm:grid-cols-4 gap-3 text-center py-2 px-2 ">         
@@ -63,11 +53,8 @@ function CollectionPage() {
         </div>
         <ToastContainer />
       </div>
-
-      <Footer></Footer>
-    </>
-  );
-
-  }
+      </>
+     );
+}
 
 export default CollectionPage
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,55 +1,21 @@
 
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Navbar({ MenuItems = [] }) {
+function Navbar() {
     return (
-        <nav class="bg-gray-800">
-            <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                <div class="relative flex h-16 items-center justify-between">
-                    <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                        <button type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-                            <span class="sr-only">Open main menu</span>
-
-                            <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                            </svg>
-                            <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                        <div class="flex flex-shrink-0 items-center">
-                            <img class="block h-12 w-auto lg:hidden" src="./q-logo-1.png" alt="Your Company" />
-                            <img class="hidden h-12 w-auto lg:block" src="./q-logo1.png" alt="Your Company" />
-                        </div>
-                        <div class="hidden sm:ml-6 sm:block">
-                            <div class="flex space-x-4">
-                                {
-                                    MenuItems.map(item => {
-                                        return (
-                                            <a href={item.url} className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" onClick={item.action}>{item.title}</a>
-                                        )
-                                    })
-                                }
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="sm:hidden" id="mobile-menu">
-                <div class="space-y-1 px-2 pt-2 pb-3">
-                    {
-                        MenuItems.map(item => {
-                            return (
-                                <a href={item.url} className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium" onClick={item.action}>{item.title}</a>
-                            )
-                        })
-                    }
-                </div>
-            </div>
+        <>
+        <nav className="bg-gray-800 justify-center">
+            <ul className="flex flex-row justify-center ">
+            <li className="p-4 hover:text-white">
+            <Link to="/">Home</Link>
+            </li>
+            <li className="p-4 hover:text-white">
+            <Link to="/collectionpage">Collection</Link>
+            </li>
+        </ul>
         </nav>
+    </>
     )
 }
 export default Navbar
