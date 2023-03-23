22f014fd7f4fb73a88b5c328dc33dd8554aaad17
Leonawesley <leonajenishlin@gmail.com>
2023-03-23 14:22:17 +0000

    change the collection page template

--- src/components/CollectionPage/CollectionPage.js
+++ src/components/CollectionPage/CollectionPage.js
@@ -21,11 +21,23 @@ function CollectionPage(){
       }
       const backgroundColor = quoteObject.color;
       return (
-        <div className=
-          "rounded-lg h-32 transition ease-in-out delay-150 border-4 border-indigo-300/50 hover:scale-110 hover:text-white hover:bg-indigo-500 duration-300 overflow-ellipsis overflow-hidden " 
-          quote-key={quoteObject.quote} 
-          style={{ backgroundColor }}>
-            <FontAwesomeIcon icon="fa-thin fa-face-smile" />{quoteObject.quote} </div>)
+        <div className="flex justify-center mt-4 rounded-lg transition ease-in-out delay-150 hover:scale-110 hover:bg-teal-500 duration-300 overflow-ellipsis overflow-hidden " quote-key={quoteObject.quote} >
+            <div className="block max-w-sm rounded-lg bg-white p-6 shadow-lg ">
+              <div class="mb-2 leading-tight">
+                <i class="fa-sharp fa-regular fa-quotes" aria-hidden="true"></i>
+              </div>
+              <p className="mb-4 text-base rounded-lg text-white p-4" style={{ backgroundColor }}>
+                  {quoteObject.quote}
+              </p>
+              <button
+                  type="button"
+                  className="inline-block rounded bg-cyan-700 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                  data-te-ripple-init
+                  data-te-ripple-color="light">
+                  Copy Quote
+              </button>
+            </div>
+        </div>)
       }
     
     return (
@@ -33,7 +45,7 @@ function CollectionPage(){
       <>
       <Navbar></Navbar>
       <div className="bg-local bg-cover background-image bg-no-repeat my-6 py-4 mx-7 shadow-xl shadow-cyan-500/50 ">
-        <h1 className=" flex justify-center text-center rounded-lg py-2 px-2 lg:ml-[700px] sm:ml-[200px] md:ml-[250px] font-bold text-xl max-w-lg bg-gradient-to-r from-indigo-600 to-pink-500 text-white">Favorite quotes</h1>
+        <h1 className=" flex justify-center text-center rounded-lg py-3 px-3 lg:ml-[700px] sm:ml-[200px] md:ml-[250px] font-bold text-xl max-w-lg bg-gradient-to-r from-emerald-600 to-teal-500 text-white">Favorite quotes</h1>
         <div className="grid grid-cols-2 sm:grid-cols-4 gap-3 text-center py-2 px-2 ">         
             {/* <div className="colourOverlay" style={{ backgroundColor }}></div> */}
             {savedQuotes.map((quote) => generatingQuote(quote))}
